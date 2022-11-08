6d8b4939c015fc9386a80174f756cbe9c16386b0
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2022-11-08 18:11:21 +0100

    Add files via upload

---
+++ Arkanoid 2.0/HolaSDL/main.cpp
@@ -0,0 +1,14 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "checkML.h"
+#include <iostream>
+
+using namespace std;
+
+using uint = unsigned int;
+
+int main(int argc, char* argv[]) 
+{
+
+	return 0;
+}
