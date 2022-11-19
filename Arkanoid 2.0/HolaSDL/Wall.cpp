526eef68ebc21b1959469c0411b800ab69e33971
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2022-11-19 17:59:18 +0100

    Wall y moving object

--- Arkanoid 2.0/HolaSDL/Wall.cpp
+++ Arkanoid 2.0/HolaSDL/Wall.cpp
@@ -1,1 +1,18 @@
+#include "checkML.h"
 #include "Wall.h"
+
+using namespace std;
+
+// Llama a renderizar la textura
+void Wall::render() const
+{
+	SDL_Rect destRect = getRect();
+	texture->render(destRect);
+}
+
+// Devuelve el vector de colision de la pelota con la pared y devuelve true en caso de producirse
+bool Wall::collides(SDL_Rect ballRect, Vector2D& collisonVector)
+{
+	collisonVector = colVector;
+	return SDL_HasIntersection(&ballRect, &getRect());
+}
